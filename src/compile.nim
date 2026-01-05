0402e7dd94ece8a93527ce65ddba32285be0cd72
RowDaBoat <reardenr@gmail.com>
2026-01-05 15:47:41 -0300

    Use shaderc_combined.lib for linking on windows.

--- src/compile.nim
+++ src/compile.nim
@@ -1,8 +1,12 @@
 import os
 
-{.passL: currentSourcePath.parentDir/"libshaderc_combined.a".}
+when defined(windows):
+  {.passL: currentSourcePath.parentDir/"shaderc_combined.lib".}
+else:
+  {.passL: currentSourcePath.parentDir/"libshaderc_combined.a".}
 
 when defined(macosx):
   {.passL: "-lc++".}
 elif defined(unix):
   {.passL: "-lstdc++".}
+
