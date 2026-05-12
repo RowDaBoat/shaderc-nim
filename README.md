6816d6ee0f3295599b70d12945dd03060d25695c
Row <reardenr@gmail.com>
2026-05-12 18:50:04 -0300

    Add a ritual.nim task script, update build requirements for Windows.

--- README.md
+++ README.md
@@ -4,16 +4,15 @@
 ### Linux
 - Python 3, and `python` must be version 3 or above.
 - c++ libs and headers, in Debian: `apt install libstdc++ libc++-dev libc++abi-dev libstdc++12-dev`
- 
+
 ### MacOS
 - XCode
 
 ### Windows (MSVC toolchain)
 - [Python 3](https://www.python.org/)
 - [Windows SDK and MSVC](https://learn.microsoft.com/en-us/windows/apps/windows-sdk/)
-- [Make](https://gnuwin32.sourceforge.net/packages/make.htm)
+- [Build Tools for Visual Studio](https://learn.microsoft.com/en-us/visualstudio/msbuild/msbuild?view=visualstudio&viewFallbackFrom=vs-2019) (MSVC + MSBuild)
 - [CMake](https://cmake.org/)
-- [Ninja](https://ninja-build.org/)
 
 ## Installation
 Just add `requires "https://github.com/RowDaBoat/shadercnim.git"` to your `.nimble` file.
---
+++ ritual.nim
@@ -0,0 +1,33 @@
+import std/[os, osproc, strutils]
+import rituals
+
+
+# TODO move this to rituals
+proc coreCount(): string =
+  when defined(windows):
+    execProcess("cmd /c echo %NUMBER_OF_PROCESSORS%").strip()
+  elif defined(macosx):
+    execProcess("sysctl -n hw.logicalcpu").strip()
+  else:
+    execProcess("nproc").strip()
+
+
+ritual "generate":
+  # Note: this ritual requires Futhark's `opir` installed.
+  nim.compile("gen/generator.nim", flags = "-r -d:futharkRebuild -d:opirRebuild", name = "generate bindings")
+  remove("gen/generator", name = "clean generator")
+
+
+ritual "build":
+  let python = when defined(macosx): "python3" else: "python"
+  when defined(windows):
+    let libPath = "shaderc/build/libshaderc/Release/shaderc_combined.lib"
+    let destPath = "src/shaderc_combined.lib"
+  else:
+    let libPath = "shaderc/build/libshaderc/libshaderc_combined.a"
+    let destPath = "src/libshaderc_combined.a"
+
+  cmd("cd shaderc && " & python & " ./utils/git-sync-deps", name = "sync deps")
+  cmd("cd shaderc && cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DSHADERC_SKIP_TESTS=ON -DSHADERC_SKIP_EXAMPLES=ON -DSHADERC_SKIP_COPYRIGHT_CHECK=ON", name = "cmake configure")
+  cmd("cd shaderc && cmake --build build --target shaderc_combined --config Release -j " & coreCount(), name = "cmake build")
+  copy(libPath, destPath, name = "install lib")
